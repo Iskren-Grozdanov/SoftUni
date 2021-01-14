f1838e2bb1fa3b16b077118a23f10309ea2f43af
iskren <asrata12@abv.bg>
2021-01-14 14:22:52 +0200

    Programming Basics exercises

---
+++ C#-Programming-Basics-2020-March/Charity Campaign/Charity Campaign.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6_charity_campaign
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int days = int.Parse(Console.ReadLine());
+            int sladkari = int.Parse(Console.ReadLine());
+            int cakes = int.Parse(Console.ReadLine());
+            int gof = int.Parse(Console.ReadLine());
+            int pal = int.Parse(Console.ReadLine());
+
+            double cakesPrice = 45.0 * cakes * sladkari * days;
+            double gofPrice = 5.80 * gof * sladkari * days;
+            double palPrice = 3.20 * pal * sladkari * days;
+            double totalPrice = cakesPrice + gofPrice + palPrice;
+
+            double costs = totalPrice / 8;
+            double sum = totalPrice - costs;
+
+            Console.WriteLine(sum);
+        }
+    }
+}
