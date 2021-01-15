f74b6fa2865b9110f85a33e9e03b54c5e8313d13
iskren <asrata12@abv.bg>
2021-01-15 17:44:23 +0200

    Added stacks and queues exercise from C# advanced jan 2021

---
+++ C#-Advanced-2021-Jan/StacksAndQueues-Exercises/05. Fashion Boutique/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _05._Fashion_Boutique
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] clothes = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int capacity = int.Parse(Console.ReadLine());
+            Stack<int> stack = new Stack<int>(clothes);
+            int racks = 0;
+            int sum = 0;
+
+            while (stack.Any())
+            {
+                int curr = stack.Peek();
+                if (sum+curr > capacity)
+                {
+                    racks++;
+                    sum = 0;
+                }
+                else if (sum + curr == capacity)
+                {
+                    racks++;
+                    sum = 0;
+                    stack.Pop();
+                }
+                else
+                {
+                    sum += stack.Pop();
+                }
+            }
+            if (sum>0)
+            {
+                racks++;
+            }
+
+            Console.WriteLine(racks);
+        }
+    }
+}
---
+++ C#-Advanced-2021-Jan/StacksAndQueues-Exercises/06. Songs Queue/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _06._Songs_Queue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] songs = Console.ReadLine().Split(", ");
+
+            Queue<string> queue = new Queue<string>(songs);
+
+            string command = Console.ReadLine();
+
+            while (queue.Any())
+            {
+                if (command.Contains("Play"))
+                {
+                    queue.Dequeue();
+                }
+                if (command.Contains("Add"))
+                {
+                    var song = command.Substring(4);  // OR command.Split("Add ", StringSplitOptions.RemoveEmptyEntries)[0];
+
+                    if (queue.Contains(song))
+                    {
+                        Console.WriteLine($"{song} is already contained!");
+                    }
+                    else
+                    {
+                        queue.Enqueue(song);
+                    }
+                }
+                if (command.Contains("Show"))
+                {
+                    Console.WriteLine(string.Join(", ", queue));
+                }
+                command = Console.ReadLine();
+            }
+            Console.WriteLine("No more songs!");
+        }
+    }
+}
