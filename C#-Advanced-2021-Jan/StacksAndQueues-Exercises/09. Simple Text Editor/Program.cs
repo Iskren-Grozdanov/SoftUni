13790f030d6f71d23854121fa7b216a9cc42a309
iskren <asrata12@abv.bg>
2021-01-16 13:16:34 +0200

    Added stacks and queues exercise from C# advanced jan 2021

--- C#-Advanced-2021-Jan/StacksAndQueues-Exercises/09. Simple Text Editor/Program.cs
+++ C#-Advanced-2021-Jan/StacksAndQueues-Exercises/09. Simple Text Editor/Program.cs
@@ -1,12 +1,52 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 
-namespace _09._Simple_Text_Editor
+namespace Test
 {
     class Program
     {
         static void Main(string[] args)
         {
-            
+            int n = int.Parse(Console.ReadLine());
+
+            var text = new StringBuilder();
+
+            Stack<string> stack = new Stack<string>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] command = Console.ReadLine().Split().ToArray();
+
+                int operation = int.Parse(command[0]);
+
+                if (operation == 1)
+                {
+                    stack.Push(text.ToString());
+                    string textToAppend = command[1];
+                    text.Append(textToAppend);
+                }
+                else if (operation == 2)
+                {
+                    stack.Push(text.ToString());
+                    int count = int.Parse(command[1]);
+                    text.Remove(text.Length - count, count);
+                }
+                else if (operation == 3)
+                {
+                    int index = int.Parse(command[1]) - 1;
+                    if (index >= 0 && index < text.Length)
+                    {
+                        Console.WriteLine(text[index]);
+                    }
+                }
+                else if (operation == 4)
+                {
+                    text.Clear();
+                    text.Append(stack.Pop());
+                }
+            }
         }
     }
 }
