759e2b27c01174ff9094be6fb0d60391633eb55f
iskren <asrata12@abv.bg>
2021-01-18 13:02:41 +0200

    Added stacks and queues exercise from C# advanced jan 2021

---
+++ C#-Advanced-2021-Jan/02.MultidimensionalArrays-Lab/5. Square With Maximum Sum/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Linq;
+
+namespace _05SquareWithMaximumSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var size = Console.ReadLine()
+                .Split(new[] { ' ', ',' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int rows = size[0];
+            int cols = size[1];
+
+            var matrix = new int[rows, cols];
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                var currentRow = Console.ReadLine()
+                    .Split(new[] { ' ', ',' }, StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToArray();
+
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = currentRow[col];
+                }
+            }
+
+            int maxSum = int.MinValue;
+            int maxSumRow = int.MinValue;
+            int maxSumCol = int.MinValue;
+
+            for (int row = 0; row < matrix.GetLength(0) - 1; row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1) - 1; col++)
+                {
+                    int currentSum = matrix[row, col] + matrix[row, col + 1]
+                        + matrix[row + 1, col] + matrix[row + 1, col + 1];
+
+                    if (currentSum > maxSum)
+                    {
+                        maxSum = currentSum;
+                        maxSumRow = row;
+                        maxSumCol = col;
+                    }
+                }
+            }
+
+            Console.WriteLine($"{matrix[maxSumRow, maxSumCol]} {matrix[maxSumRow, maxSumCol + 1]}");
+            Console.WriteLine($"{matrix[maxSumRow + 1, maxSumCol]} {matrix[maxSumRow + 1, maxSumCol + 1]}");
+            Console.WriteLine(maxSum);
+        }
+    }
+}
