efc612186ad38128718b1c942907ef3358573669
iskren <asrata12@abv.bg>
2021-01-19 16:11:08 +0200

    Test

---
+++ C#-Advanced-2021-Jan/02.MultidimensionalArrays-Exercises/MultidimensionalArrays-Exercises/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Linq;
+
+namespace _1._Diagonal_Difference
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
