a904f69319fe4db12f32f0426ff46018cc7592f5
iskren <asrata12@abv.bg>
2021-01-19 16:08:48 +0200

    Added stacks and queues exercise from C# advanced jan 2021

---
+++ C#-Advanced-2021-Jan/02.MultidimensionalArrays-Exercises/1. Diagonal Difference/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Linq;
+
+namespace _1._Diagonal_Difference
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int[,] matrix = new int[n, n];
+            int sumFirstDiagonal = 0;
+            int sumSecondDiagonal = 0;
+
+            FillsMatrix(matrix);
+
+            // First Diagonal
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    if (row == col)
+                    {
+                        sumFirstDiagonal += matrix[row, col];
+                    }
+                    if (col == n - 1 - row)
+                    {
+                        sumSecondDiagonal += matrix[row, col];
+                    }
+                }
+            }
+
+            Console.WriteLine(Math.Abs(sumFirstDiagonal - sumSecondDiagonal));
+
+        }
+
+        private static void PrintMatrix(int[,] matrix)
+        {
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    Console.Write(matrix[row, col] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        private static void FillsMatrix(int[,] matrix)
+        {
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+
+                int[] input = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = input[col];
+                }
+            }
+        }
+    }
+}
