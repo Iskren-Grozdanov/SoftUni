e4eb9b87d2f610b387ae6ba3c8b5f2a2aff6ef7a
iskren <asrata12@abv.bg>
2021-01-20 17:14:01 +0200

    Added stacks and queues exercise from C# advanced jan 2021

---
+++ C#-Advanced-2021-Jan/02.MultidimensionalArrays-Exercises/2. Squares in Matrix/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Linq;
+
+namespace _2._Squares_in_Matrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] size = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            char[,] matrix = new char[size[0], size[1]];
+
+            FillsMatrix(matrix);
+
+            int count = 0;
+
+            for (int row = 0; row < matrix.GetLength(0) - 1; row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1) - 1; col++)
+                {
+                    char currElement = matrix[row, col];
+                    if (currElement == matrix[row,col+1] && currElement == matrix[row+1,col] && currElement == matrix[row+1, col+1] )
+                    {
+                        count++;
+                    }
+                }
+            }
+
+            Console.WriteLine(count);
+
+
+        }
+
+        private static void PrintMatrix(char[,] matrix)
+        {
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    Console.Write(matrix[row, col] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        private static void FillsMatrix(char[,] matrix)
+        {
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                char[] input = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(char.Parse)
+                .ToArray();
+
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = input[col];
+                }
+            }
+        }
+    }
+}
