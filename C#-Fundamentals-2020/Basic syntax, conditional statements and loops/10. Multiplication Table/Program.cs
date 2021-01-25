71d7369c59c08fc137f15044ed097558d08d14aa
iskren <asrata12@abv.bg>
2021-01-25 23:34:15 +0200

    Added stacks and queues exercise from C# advanced jan 2021

---
+++ C#-Fundamentals-2020/Basic syntax, conditional statements and loops/10. Multiplication Table/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _10._Multiplication_Table
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            int sum = 0;
+
+            for (int start = 1; start <= 10; start++)
+            {
+                sum = start * number;
+                Console.WriteLine($"{number} X {start} = {sum}");
+            }
+        }
+    }
+}
