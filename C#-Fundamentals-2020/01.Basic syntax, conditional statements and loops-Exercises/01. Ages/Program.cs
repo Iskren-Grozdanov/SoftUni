c247075e59283f628cb8c964859b482f3d173dda
iskren <asrata12@abv.bg>
2021-01-26 16:34:25 +0200

    Added stacks and queues exercise from C# advanced jan 2021

---
+++ C#-Fundamentals-2020/01.Basic syntax, conditional statements and loops-Exercises/01. Ages/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace _01._Ages
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = int.Parse(Console.ReadLine());
+            string name = "";
+            if (age < 0)
+            {
+
+            }
+            else if (age <= 2)
+            {
+                name = "baby";
+            }
+            else if (age > 2 && age <= 13)
+            {
+                name = "child";
+            }
+            else if (age > 13 && age <= 19)
+            {
+                name = "teenager";
+            }
+            else if (age > 19 && age <= 65)
+            {
+                name = "adult";
+            }
+            else if (age > 65)
+            {
+                name = "elder";
+            }
+            Console.WriteLine(name);
+        }
+    }
+}
