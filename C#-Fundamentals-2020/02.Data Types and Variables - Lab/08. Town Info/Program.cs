6f810202f674f0eac5ee2288af32f9ee1a2d8e5c
iskren <asrata12@abv.bg>
2021-01-31 22:03:15 +0200

    Added exercise from C# fundamentals sep 2020

---
+++ C#-Fundamentals-2020/02.Data Types and Variables - Lab/08. Town Info/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _08._Town_Info
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string town = Console.ReadLine();
+            int num = int.Parse(Console.ReadLine());
+            int km = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Town {town} has population of {num} and area {km} square km.");
+        }
+    }
+}
