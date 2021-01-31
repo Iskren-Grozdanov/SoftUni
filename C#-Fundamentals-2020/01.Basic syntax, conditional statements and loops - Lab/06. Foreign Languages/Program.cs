2ba9708ead1895399c1d03ea65e0daafd71ae0a0
iskren <asrata12@abv.bg>
2021-01-31 22:04:46 +0200

    Added exercise from C# fundamentals sep 2020

---
+++ C#-Fundamentals-2020/01.Basic syntax, conditional statements and loops - Lab/06. Foreign Languages/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _06._Foreign_Languages
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // English - "England", "USA"
+            // Spanish - "Spain", "Argentina", "Mexico"
+            // Others - print "unknown"
+
+            string country = Console.ReadLine();
+
+            switch (country)
+            {
+                case "USA":
+                case "England":
+                    Console.WriteLine("English");
+                    break;
+                case "Spain":
+                case "Argentina":
+                case "Mexico":
+                    Console.WriteLine("Spanish");
+                    break;
+                default:
+                    Console.WriteLine("unknown");
+                    break;
+            }
+        }
+    }
+}
