6bde9e16a8a4c4640e64940c87f99fe8f058a80f
iskren <asrata12@abv.bg>
2021-01-31 22:04:04 +0200

    Added exercise from C# fundamentals sep 2020

---
+++ C#-Fundamentals-2020/02.Data Types and Variables - Lab/12. Refactor Special Numbers/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _12._Refactor_Special_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int input = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= input; i++)
+            {
+                int sum = 0;
+                int currNum = i;
+
+                while (currNum > 0)
+                {
+                    sum += currNum % 10;
+                    currNum /= 10;
+                }
+
+                bool isSpecial = (sum == 5) || (sum == 7) || (sum == 11);
+
+                Console.WriteLine("{0} -> {1}", i, isSpecial);
+            }
+        }
+    }
+}
