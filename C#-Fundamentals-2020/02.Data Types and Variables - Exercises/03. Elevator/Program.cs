910b28569e5c8f51ef92aaf811375bbaddeb4cf1
iskren <asrata12@abv.bg>
2021-02-01 23:35:59 +0200

    Added exercise from C# fundamentals sep 2020

---
+++ C#-Fundamentals-2020/02.Data Types and Variables - Exercises/03. Elevator/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _03._Elevator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int perople = int.Parse(Console.ReadLine());
+            int capacity = int.Parse(Console.ReadLine());
+
+            int fullCourses = (int)Math.Ceiling((double)perople / capacity);
+            Console.WriteLine(fullCourses);
+
+
+        }
+    }
+}
