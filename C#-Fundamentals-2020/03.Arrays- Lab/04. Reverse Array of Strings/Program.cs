31b2475fb55f3871e89a24016b0a53bece66ef38
iskren <asrata12@abv.bg>
2021-02-02 23:50:58 +0200

    Added exercise from C# fundamentals sep 2020

---
+++ C#-Fundamentals-2020/03.Arrays- Lab/04. Reverse Array of Strings/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+
+namespace _04._Reverse_Array_of_Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(String.Join(' ', Console.ReadLine()
+                .Split()
+                .Reverse()));
+        }
+    }
+}
