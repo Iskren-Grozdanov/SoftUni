fe00f9f3bd3c4d46bbad9983f98bfbcc9c3caa09
iskren <asrata12@abv.bg>
2021-02-02 23:50:28 +0200

    Added exercise from C# fundamentals sep 2020

---
+++ C#-Fundamentals-2020/03.Arrays- Lab/02. Print Numbers in Reverse Order/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace _02._Print_Numbers_in_Reverse_Order
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] array = new int[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                array[i] = int.Parse(Console.ReadLine());
+            }
+
+            Console.Write(String.Join(' ', array.Reverse()));
+        }
+    }
+}
