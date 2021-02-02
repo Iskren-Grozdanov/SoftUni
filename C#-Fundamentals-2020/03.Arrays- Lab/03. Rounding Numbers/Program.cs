ad06079042c8f94c580c6bafaa032e664024388f
iskren <asrata12@abv.bg>
2021-02-02 23:50:48 +0200

    Added exercise from C# fundamentals sep 2020

---
+++ C#-Fundamentals-2020/03.Arrays- Lab/03. Rounding Numbers/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace _03._Rounding_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] array = Console.ReadLine()
+                .Split()
+                .Select(double.Parse)
+                .ToArray();
+
+            foreach (double number in array)
+            {
+                int rounded = (int)Math.Round(number, MidpointRounding.AwayFromZero);
+                Console.WriteLine("{0} => {1}", number, rounded);
+            }
+        }
+    }
+}
