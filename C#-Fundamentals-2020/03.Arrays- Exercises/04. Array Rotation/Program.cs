9b406aa6d51dd8857a0b4ac4ba902285fb2e6ff9
iskren <asrata12@abv.bg>
2021-02-03 23:15:10 +0200

    Added exercise from C# fundamentals sep 2020

---
+++ C#-Fundamentals-2020/03.Arrays- Exercises/04. Array Rotation/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _04._Array_Rotation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] arr = Console.ReadLine().Split();
+            int n = int.Parse(Console.ReadLine());
+
+            for (int c = 0; c < n; c++)
+            {
+                string temp = arr[0];
+                for (int i = 0; i < arr.Length - 1; i++)
+                {
+                    arr[i] = arr[i + 1];
+                }
+                arr[arr.Length - 1] = temp;
+            }
+
+            Console.WriteLine(String.Join(" ", arr));
+        }
+    }
+}
