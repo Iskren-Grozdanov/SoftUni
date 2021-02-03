099cafda87b1ef6cd7ed621fa8e86cabbb8fd558
iskren <asrata12@abv.bg>
2021-02-03 23:21:11 +0200

    Added exercise from C# fundamentals sep 2020

---
+++ C#-Fundamentals-2020/04.Methods - Lab/04. Printing Triangle/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _04._Printing_Triangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
