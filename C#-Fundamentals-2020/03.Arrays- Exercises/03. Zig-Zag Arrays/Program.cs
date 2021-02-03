1a212daa727f1b5601c98f5d4f36dd3eecddbc9a
iskren <asrata12@abv.bg>
2021-02-03 23:14:58 +0200

    Added exercise from C# fundamentals sep 2020

---
+++ C#-Fundamentals-2020/03.Arrays- Exercises/03. Zig-Zag Arrays/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+
+namespace _03._Zig_Zag_Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            string[] array1 = new string[n];
+            string[] array2 = new string[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] currArray = Console.ReadLine()
+                    .Split();
+
+                if (i % 2 == 0)
+                {
+                    array1[i] = currArray[0];
+                    array2[i] = currArray[1];
+                }
+                else
+                {
+                    array1[i] = currArray[1];
+                    array2[i] = currArray[0];
+                }
+            }
+            Console.WriteLine(String.Join(" ", array1));
+            Console.WriteLine(String.Join(" ", array2));
+        }
+    }
+}
