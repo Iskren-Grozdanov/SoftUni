f39f4a26ecc0ed7e60cd22095414a8fbec9052fc
iskren <asrata12@abv.bg>
2021-02-03 23:14:19 +0200

    Added exercise from C# fundamentals sep 2020

---
+++ C#-Fundamentals-2020/03.Arrays- Exercises/01. Train/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _01._Train
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int[] arr = new int[n];
+            int sum = 0;
+            int i = 0;
+            for (i = 0; i < n; i++)
+            {
+                arr[i] = int.Parse(Console.ReadLine());
+                sum += arr[i];
+            }
+
+            Console.WriteLine($"{String.Join(' ', arr)} ");
+            Console.WriteLine(sum);
+        }
+    }
+}
