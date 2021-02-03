c426be996dddda6bea577d1a96ce37b78287945d
iskren <asrata12@abv.bg>
2021-02-03 23:14:45 +0200

    Added exercise from C# fundamentals sep 2020

---
+++ C#-Fundamentals-2020/03.Arrays- Exercises/02. Common Elements/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+
+namespace _02._Common_Elements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] text1 = Console.ReadLine().Split(" ");
+            string[] text2 = Console.ReadLine().Split(" ");
+
+            foreach (string item in text2)
+            {
+
+                foreach (string item2 in text1)
+                {
+                    if (item == item2)
+                    {
+
+                        Console.Write(item + " ");
+                        break;
+
+                    }
+                }
+            }
+
+
+        }
+    }
+}
