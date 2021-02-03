cbcd1fa26d88db34386004a9d9fcc05f48a8b362
iskren <asrata12@abv.bg>
2021-02-03 23:15:17 +0200

    Added exercise from C# fundamentals sep 2020

---
+++ C#-Fundamentals-2020/03.Arrays- Exercises/05. Top Integers/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+
+namespace _05._Top_Integers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+            string result = "";
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                int curr = array[i];
+                bool isTopInteger = true;
+
+                for (int index = i + 1; index < array.Length; index++)
+                {
+                    if (curr <= array[index])
+                    {
+                        isTopInteger = false;
+                        break;
+                    }
+                }
+                if (isTopInteger)
+                {
+                    result += curr + " ";
+                }
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
