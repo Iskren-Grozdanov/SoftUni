003289ad05c2999e3b384c4b9dc98e6bd27b25eb
iskren <asrata12@abv.bg>
2021-02-04 22:24:09 +0200

    Added exercise from C# fundamentals sep 2020

---
+++ C#-Advanced-2021-Jan/03.SetsAndDictionaries-Lab/3. Product Shop/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _3._Product_Shop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            Dictionary<string, Dictionary<string, double>> dictionary = new Dictionary<string, Dictionary<string, double>>();
+
+
+            while (input != "Revision")
+            {
+                string[] inputArgs = input.Split(", ", StringSplitOptions.RemoveEmptyEntries);
+
+                string market = inputArgs[0];
+                string item = inputArgs[1];
+                double price = double.Parse(inputArgs[2]);
+
+                if (!dictionary.ContainsKey(market))
+                {
+                    dictionary.Add(market, new Dictionary<string, double>());
+                }
+
+                dictionary[market].Add(item, price);
+
+                input = Console.ReadLine();
+            }
+
+            foreach (var (market, itemsAndPrice) in dictionary.OrderBy(x => x.Key))
+            {
+                Console.WriteLine($"{market}->");
+
+                foreach (var (item, price) in itemsAndPrice)
+                {
+                    Console.WriteLine($"Product: {item}, Price: {price}");
+                }
+            }
+        }
+    }
+}
