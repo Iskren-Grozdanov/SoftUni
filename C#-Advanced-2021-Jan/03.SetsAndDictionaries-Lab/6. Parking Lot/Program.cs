589df44c9fde64daf29f1e2535725916c8bbb1b8
iskren <asrata12@abv.bg>
2021-02-04 22:24:42 +0200

    Added exercise from C# fundamentals sep 2020

---
+++ C#-Advanced-2021-Jan/03.SetsAndDictionaries-Lab/6. Parking Lot/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _6._Parking_Lot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> set = new HashSet<string>();
+
+            string input = Console.ReadLine();
+
+            while (input != "END")
+            {
+                string[] inputArgs = input.Split(", ", StringSplitOptions.RemoveEmptyEntries);
+
+                string command = inputArgs[0];
+                string number = inputArgs[1];
+
+                if (command == "IN")
+                {
+                    set.Add(number);
+                }
+                else
+                {
+                    set.Remove(number);
+                }
+
+                input = Console.ReadLine();
+            }
+
+            if (!set.Any())
+            {
+                Console.WriteLine("Parking Lot is Empty");
+                return;
+            }
+
+            foreach (var item in set)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
