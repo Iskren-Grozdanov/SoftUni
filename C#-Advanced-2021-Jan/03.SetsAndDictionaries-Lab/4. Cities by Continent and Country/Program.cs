ddcf811ca213def375a7fedcdd0c41664190c096
iskren <asrata12@abv.bg>
2021-02-04 22:24:24 +0200

    Added exercise from C# fundamentals sep 2020

---
+++ C#-Advanced-2021-Jan/03.SetsAndDictionaries-Lab/4. Cities by Continent and Country/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _4._Cities_by_Continent_and_Country
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Dictionary<string, Dictionary<string, List<string>>> dictionary = new Dictionary<string, Dictionary<string, List<string>>>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] input = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                string continent = input[0];
+                string country = input[1];
+                string city = input[2];
+
+                if (!dictionary.ContainsKey(continent))
+                {
+                    dictionary.Add(continent, new Dictionary<string, List<string>>());
+                }
+                if (!dictionary[continent].ContainsKey(country))
+                {
+                    dictionary[continent][country] = new List<string>();
+                }
+
+                dictionary[continent][country].Add(city);
+            }
+
+            foreach (var (continent, countryAndCity) in dictionary)
+            {
+                Console.WriteLine($"{continent}:");
+
+                foreach (var (country, city) in countryAndCity)
+                {
+                    Console.WriteLine($"  {country} -> {string.Join(", ", city)}");
+                }
+            }
+        }
+    }
+}
