025fb8a0b9e3d268b358faf0c7aedfd3c21a7b99
iskren <asrata12@abv.bg>
2021-02-04 14:08:47 +0200

    Added exercise from C# fundamentals sep 2020

---
+++ C#-Fundamentals-2020/04.Methods - Lab/07. Repeat String/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _07._Repeat_String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
