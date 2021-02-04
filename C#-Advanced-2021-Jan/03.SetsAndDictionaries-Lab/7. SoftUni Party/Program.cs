81274b8cce4557bdc869ec62b65c553b5f805a85
iskren <asrata12@abv.bg>
2021-02-04 22:24:50 +0200

    Added exercise from C# fundamentals sep 2020

---
+++ C#-Advanced-2021-Jan/03.SetsAndDictionaries-Lab/7. SoftUni Party/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _07SoftUniParty
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> vip = new HashSet<string>();
+
+            HashSet<string> regular = new HashSet<string>();
+
+            string command = Console.ReadLine();
+
+            while (command != "PARTY")
+            {
+                foreach (var symbol in command)
+                {
+                    if (Char.IsDigit(symbol))
+                    {
+                        vip.Add(command);
+                    }
+                    else
+                    {
+                        regular.Add(command);
+                    }
+                    break;
+                }
+                command = Console.ReadLine();
+            }
+
+            string input = Console.ReadLine();
+
+            while (input != "END")
+            {
+                if (vip.Contains(input))
+                {
+                    vip.Remove(input);
+                }
+                else
+                {
+                    regular.Remove(input);
+                }
+
+                input = Console.ReadLine();
+            }
+
+            int sum = vip.Count() + regular.Count();
+
+            Console.WriteLine(sum);
+
+            foreach (var item in vip)
+            {
+                Console.WriteLine(item);
+            }
+            foreach (var item in regular)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
