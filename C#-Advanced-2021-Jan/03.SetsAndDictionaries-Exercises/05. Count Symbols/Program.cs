d40a52b83452b6201c47f82009d9e01a75d3fe7f
iskren <asrata12@abv.bg>
2021-02-05 01:07:26 +0200

    Added exercise from C# fundamentals sep 2020

---
+++ C#-Advanced-2021-Jan/03.SetsAndDictionaries-Exercises/05. Count Symbols/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _05._Count_Symbols
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            SortedDictionary<char, int> dictionary = new SortedDictionary<char, int>();
+
+            foreach (var symbol in input)
+            {
+                if (!dictionary.ContainsKey(symbol))
+                {
+                    dictionary.Add(symbol,0);
+                }
+                dictionary[symbol]++;
+            } 
+
+            foreach (var (symbol, times) in dictionary)
+            {
+                Console.WriteLine($"{symbol}: {times} time/s");
+            }
+        }
+    }
+}
