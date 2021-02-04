77e11955629da386f9bc16867f68999f1af4993b
iskren <asrata12@abv.bg>
2021-02-04 14:08:55 +0200

    Added exercise from C# fundamentals sep 2020

---
+++ C#-Fundamentals-2020/04.Methods - Lab/08. Math Power/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _08._Math_Power
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
