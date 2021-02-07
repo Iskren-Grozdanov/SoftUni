309f67d93bb97dd2d2ac353f80e8458b6b672dd3
iskren <asrata12@abv.bg>
2021-02-07 18:57:31 +0200

    Added exercise from C# Advanced Jan 2021

---
+++ C#-Advanced-2021-Jan/05.FunctionalProgramming-Exercises/01. Action Print/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01._Action_Print
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Action<string[]> PrintOnNewLine = item => Console.WriteLine(string.Join(Environment.NewLine, item));
+
+            string[] words = Console.ReadLine()
+                .Split();
+
+            PrintOnNewLine(words);
+        }
+    }
+}
