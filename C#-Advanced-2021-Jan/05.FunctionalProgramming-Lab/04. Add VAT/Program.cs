0215842e44c3d5a2134587e503e4b292cd8e6ad7
iskren <asrata12@abv.bg>
2021-02-07 18:56:45 +0200

    Added exercise from C# Advanced Jan 2021

---
+++ C#-Advanced-2021-Jan/05.FunctionalProgramming-Lab/04. Add VAT/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04._Add_VAT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ReadLine()
+                .Split(new string[] {", "}, StringSplitOptions.RemoveEmptyEntries)
+                .Select(e => {
+                    decimal num = decimal.Parse(e);     // .Select(e => e * 1.2)
+                    return num * 1.2M;
+                    })
+                .ToList()
+                .ForEach(e => Console.WriteLine($"{e:f2}"));
+        }
+    }
+}
