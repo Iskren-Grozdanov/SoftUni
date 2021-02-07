29df0d2090054c59f079b96c7826793946ae94e4
iskren <asrata12@abv.bg>
2021-02-07 18:56:24 +0200

    Added exercise from C# Advanced Jan 2021

---
+++ C#-Advanced-2021-Jan/05.FunctionalProgramming-Lab/02. Sum Numbers/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02._Sum_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<string, int> parser = int.Parse;
+
+            List<int> input = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(parser)
+                .ToList();
+
+            Console.WriteLine(input.Count());
+            Console.WriteLine(input.Sum());
+        }
+    }
+}
