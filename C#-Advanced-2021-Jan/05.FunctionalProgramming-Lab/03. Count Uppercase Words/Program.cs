e9a2d9dc8eb7e8f1dae636108475350e3ceb7aab
iskren <asrata12@abv.bg>
2021-02-07 18:56:33 +0200

    Added exercise from C# Advanced Jan 2021

---
+++ C#-Advanced-2021-Jan/05.FunctionalProgramming-Lab/03. Count Uppercase Words/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace _03CountUppercaseWords
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Predicate<string> CapitalChecker = str => str[0] == str.ToUpper()[0];
+
+            Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Where(x => CapitalChecker(x))
+                .ToList()
+                .ForEach(x => Console.WriteLine(x));   
+        }
+    }
+}
