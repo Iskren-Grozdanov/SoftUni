c4289078f1d09b843ac130e3c315bad19899d95d
iskren <asrata12@abv.bg>
2021-02-08 23:22:04 +0200

    Added exercise from C# fundamentals sep 2020

---
+++ C#-Fundamentals-2020/04.Methods - Exercises/02. Vowels Count/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _02._Vowels_Count
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = Console.ReadLine().ToLower();
+            int counter = 0;
+            for (int i = 0; i < word.Length; i++)
+            {
+
+                char currChar = word[i];
+
+                if (currChar == 'a' || currChar == 'e' || currChar == 'i' || currChar == 'o' || currChar == 'u' || currChar == 'y')
+                {
+                    counter++;
+                }
+            }
+
+            Console.WriteLine(counter);
+        }
+    }
+}
