29c43972324a88dbbc7f83e1baa49fb7e34ee4d2
iskren <asrata12@abv.bg>
2021-02-08 23:21:47 +0200

    Added exercise from C# fundamentals sep 2020

---
+++ C#-Fundamentals-2020/04.Methods - Exercises/01. Smallest of Three Numbers/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _01._Smallest_of_Three_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNum = int.Parse(Console.ReadLine());
+            int SecondNum = int.Parse(Console.ReadLine());
+            int ThirdNum = int.Parse(Console.ReadLine());
+
+            PrintBiggest(firstNum, SecondNum, ThirdNum);
+
+        }
+
+        static void PrintBiggest(int firstNum, int secondNum, int thirdNum)
+        {
+            int larger = 0;
+            if (firstNum <= secondNum && firstNum <= thirdNum)
+            {
+                larger = firstNum;
+            }
+            else if (secondNum <= firstNum && secondNum <= thirdNum)
+            {
+                larger = secondNum;
+            }
+            else if (thirdNum <= firstNum && thirdNum <= secondNum)
+            {
+                larger = thirdNum;
+            }
+            Console.WriteLine(larger);
+        }
+    }
+}
