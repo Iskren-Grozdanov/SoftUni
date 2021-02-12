3ee8b67d7e93ad010c2ae723b6978681b0e3b62c
iskren <asrata12@abv.bg>
2021-02-12 22:24:32 +0200

    Added exercise from C# fundamentals sep 2020

---
+++ C#-Fundamentals-2020/06.ObjectsAndClasses - Lab/04Songs/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _04Songs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
