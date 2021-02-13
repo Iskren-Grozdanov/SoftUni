987fba273122f3e1ff263ba93b373b32b15482d7
iskren <asrata12@abv.bg>
2021-02-13 18:49:22 +0200

    Added exercise from C# fundamentals sep 2020

---
+++ C#-Fundamentals-2020/07.AssociativeArrays - Lab/03WordSynonyms/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _03WordSynonyms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
