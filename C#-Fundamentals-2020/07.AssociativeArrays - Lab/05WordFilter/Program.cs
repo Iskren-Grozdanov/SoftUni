7f75506c52a9c40e4bbb7a5cb8550020ad4bd4b7
iskren <asrata12@abv.bg>
2021-02-13 18:49:35 +0200

    Added exercise from C# fundamentals sep 2020

---
+++ C#-Fundamentals-2020/07.AssociativeArrays - Lab/05WordFilter/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _05WordFilter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
