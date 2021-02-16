d5fb160c6a72fe1371aeb5ba8ac181dc3581e8b3
iskren <asrata12@abv.bg>
2021-02-16 23:44:03 +0200

    Added exercise from C# fundamentals sep 2020

---
+++ C#-Fundamentals-2020/08.TextProcessing - Exercises/10RageQuit/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _10RageQuit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
