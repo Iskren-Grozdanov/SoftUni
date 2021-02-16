2d616b8c691cbeba28ec1f37c721675cc8e6c87d
iskren <asrata12@abv.bg>
2021-02-16 23:43:48 +0200

    Added exercise from C# fundamentals sep 2020

---
+++ C#-Fundamentals-2020/08.TextProcessing - Exercises/08MatchNumbers/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _08MatchNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
