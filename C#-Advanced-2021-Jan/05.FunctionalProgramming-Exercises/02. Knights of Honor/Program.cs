f82152dc26674c0a5ad5eae2e2e4fbf6c23e9a7c
iskren <asrata12@abv.bg>
2021-02-18 23:25:57 +0200

    Added exercise from C# advanced jan 2021

---
+++ C#-Advanced-2021-Jan/05.FunctionalProgramming-Exercises/02. Knights of Honor/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02._Knights_of_Honor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Action<string> action = item => Console.WriteLine($"Sir {string.Join(Environment.NewLine, item)}");
+
+            Console.ReadLine()
+                .Split()
+                .ToList()
+                .ForEach(action);
+        }
+    }
+}
