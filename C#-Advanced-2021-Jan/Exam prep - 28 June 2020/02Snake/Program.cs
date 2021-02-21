c9edbd57ff13935b707aea9bda1b87cd9db6bc87
iskren <asrata12@abv.bg>
2021-02-21 20:44:29 +0200

    Exam Preparation exercise

---
+++ C#-Advanced-2021-Jan/Exam prep - 28 June 2020/02Snake/Program.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Linq;
+
+namespace Snake
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            char[,] matrix = new char[n, n];
+
+            int snakeRow = -1;
+            int snakeCol = -1;
+            int food = 0;
+            bool gameOver = false;
+
+            for (int row = 0; row < n; row++)
+            {
+                string input = Console.ReadLine();
+
+                for (int col = 0; col < n; col++)
+                {
+                    matrix[row, col] = input[col];
+                    if (matrix[row, col] == 'S')
+                    {
+                        snakeRow = row;
+                        snakeCol = col;
+                    }
+                }
+            }
+
+            while (food < 10)
+            {
+                matrix[snakeRow, snakeCol] = '.';
+                string command = Console.ReadLine();
+
+                switch (command)
+                {
+                    case "up":
+                        snakeRow--;
+                        break;
+                    case "down":
+                        snakeRow++;
+                        break;
+                    case "left":
+                        snakeCol--;
+                        break;
+                    case "right":
+                        snakeCol++;
+                        break;
+                }
+
+                if (!IsPositionValid(snakeRow,snakeCol,n,n))
+                {
+                    gameOver = true;
+                    break;
+                }
+                else
+                {
+                    if (matrix[snakeRow, snakeCol] == '*')
+                    {
+                        food++;
+                    }
+
+                    if (matrix[snakeRow, snakeCol] == 'B')
+                    {
+                        matrix[snakeRow, snakeCol] = '.';
+                        for (int row = 0; row < matrix.GetLength(0); row++)
+                        {
+                            for (int col = 0; col < matrix.GetLength(1); col++)
+                            {
+                                if (matrix[row, col] == 'B')
+                                {
+                                    snakeRow = row;
+                                    snakeCol = col;
+                                }
+                            }
+                        }
+                    }
+                }
+                
+                matrix[snakeRow, snakeCol] = 'S';
+            }
+
+            if (gameOver)
+            {
+                Console.WriteLine("Game over!");
+            }
+            else
+            {
+                Console.WriteLine("You won! You fed the snake.");
+            }
+            Console.WriteLine($"Food eaten: {food}");
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    Console.Write(matrix[row, col]);
+                }
+                Console.WriteLine();
+            }
+        }
+        public static bool IsPositionValid(int row, int col, int rows, int cols)
+        {
+            if (row < 0 || row >= rows)
+            {
+                return false;
+            }
+            if (col < 0 || col >= cols)
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
