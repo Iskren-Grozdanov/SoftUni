f9fd6642571a32fe2c76cb8d43b770518c8b62c1
iskren <asrata12@abv.bg>
2021-02-24 23:22:13 +0200

    Added exercise from C# advanced jan 2021

---
+++ C#-Advanced-2021-Jan/06.Defining Classes - Exercise/03. Oldest Family Member/Family.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace DefiningClasses
+{
+
+    public class Family
+    {
+        public List<Person> People { get; set; } = new List<Person>();
+
+        public void AddMember(Person member)
+        {
+            People.Add(member);
+        }
+
+        public Person GetOldestMember()
+        {
+            return People.OrderByDescending(x => x.Age).FirstOrDefault();
+        }
+    }
+}
