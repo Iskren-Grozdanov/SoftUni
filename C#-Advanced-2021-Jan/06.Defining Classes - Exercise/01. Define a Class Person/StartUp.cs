d944dff241293b1f8a14fee6e4c8951b5ce8ee69
iskren <asrata12@abv.bg>
2021-02-24 23:21:54 +0200

    Added exercise from C# advanced jan 2021

---
+++ C#-Advanced-2021-Jan/06.Defining Classes - Exercise/01. Define a Class Person/StartUp.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Person pesho = new Person("Pesho", 20);
+            Console.WriteLine($"{pesho.Name} {pesho.Age}");
+
+            var gosho = new Person("Gosho", 18);
+            Console.WriteLine($"{gosho.Name} {gosho.Age}");
+
+            var stamat = new Person("Stamat", 43);
+            Console.WriteLine($"{stamat.Name} {stamat.Age}");
+
+        }
+    }
+}
